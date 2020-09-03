d3c5c03998ad35e75923862d496f458395246662
Joe Fioresi <67609421+joefioresi718@users.noreply.github.com>
2020-09-03 16:13:58 -0400

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,64 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
+
+	// connect to database
+	$conn = new mysqli("localhost", "thisisthegroup@crm-group12.com", "thisisthegroup", "komilak_COP4331");
+	// check connection
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		// get ID, names after login
+		$sql = "SELECT ID,FirstName,LastName FROM Users where Username='" . $inData["login"] . "' and Password='" . $inData["password"] . "'";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0)
+		{
+			$row = $result->fetch_assoc();
+			$firstName = $row["FirstName"];
+			$lastName = $row["LastName"];
+			$id = $row["ID"];
+
+			returnWithInfo($firstName, $lastName, $id );
+		}
+		else
+		{
+			returnWithError( "No Records Found" );
+		}
+		$conn->close();
+	}
+
+	// Get username and password
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	// prints out json object
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	// prints out error to json
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	// prints out the id and names as json
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
