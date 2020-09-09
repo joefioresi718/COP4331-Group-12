6dc1e8130498fa02802ea991d8dc0ae505c6ec77
Federico Baron <federicoagustinbaron@gmail.com>
2020-09-09 18:53:26 -0400

    Fixed up frontend

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import "./styles.css";
 
-// document.getElementById("app").innerHTML = `
-// <h1>COP4331 Project</h1>
-// <div>
-//    We use the same configuration as Parcel to bundle this sandbox, you can find more
-//    info about Parcel
-//    <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-//  </div>
-// `;
-
 const signupButton = document.getElementById("signUp");
 const signinButton = document.getElementById("signIn");
 const container = document.getElementById("container");
