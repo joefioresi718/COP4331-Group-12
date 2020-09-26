8baeb864e8a2e0b528288de89490f638b286960b
Federico Baron <federicoagustinbaron@gmail.com>
2020-09-26 19:57:38 -0400

    Empty add input boxes

---
+++ src/welcome.js
@@ -0,0 +1,7 @@
+function clearInputFields()
+{
+    document.getElementById("firstNameContact").value = "";
+	document.getElementById("lastNameContact").value = "";
+	document.getElementById("emailContact").value = "";
+    document.getElementById("phoneContact").value = "";   
+}
